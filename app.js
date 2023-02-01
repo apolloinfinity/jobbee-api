e6a3911cf374dbc1cdc081c4baaa9c7799ddad0e
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2023-02-01 10:07:27 -0800

    Installed Nodemon and set up prod and dev env variables.

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ dotenv.config({ path: "./config/config.env" });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server started on port: ${process.env.PORT}`);
+  console.log(
+    `Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
 });
