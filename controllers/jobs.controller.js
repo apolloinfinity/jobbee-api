f7c11cc2dce5a70a88f88f15929730cc61865c08
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2023-02-16 23:25:00 -0800

    All jobs show in Postman.

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -1,10 +1,12 @@
 const Job = require("../models/jobs");
 // Get all Jobs => /api/v1/jobs
 
-exports.getJobs = (req, res, next) => {
+exports.getJobs = async (req, res, next) => {
+  const jobs = await Job.find();
   res.status(200).json({
     success: true,
-    message: "This route will display all jobs in the future.",
+    results: jobs.length,
+    data: jobs,
   });
 };
 
