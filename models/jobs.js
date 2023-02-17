ee23c18ec10917464354c776e13af3176ee6a0e8
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2023-02-16 23:53:09 -0800

    Set up Geocoder with a mapping API.

--- models/jobs.js
+++ models/jobs.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const slugify = require("slugify");
+const geoCoder = require("../utils/geocoder");
 
 const jobSchema = mongoose.Schema({
   title: {
@@ -22,6 +23,21 @@ const jobSchema = mongoose.Schema({
   address: {
     type: String,
     required: [true, "Please add an address."],
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dSphere",
+    },
+    formattedAddress: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
   },
   company: {
     type: String,
@@ -97,6 +113,22 @@ const jobSchema = mongoose.Schema({
 jobSchema.pre("save", function (next) {
   // creating slug before saving to DB
   this.slug = slugify(this.title, { lower: true });
+  next();
+});
+
+// setting up location
+jobSchema.pre("save", async function (next) {
+  const loc = await geoCoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
   next();
 });
 
