b6ec4b75f7d64556f334cb13ba7cc767d65ea627
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2023-02-16 23:13:49 -0800

    Created Post route for new job creation.

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -1,7 +1,19 @@
+const Job = require("../models/jobs");
 // Get all Jobs => /api/v1/jobs
+
 exports.getJobs = (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "This route will display all jobs in the future.",
+  });
+};
+
+// Create a new job => /api/v1/job
+exports.newJob = async (req, res, next) => {
+  const job = await Job.create(req.body);
+  res.status(200).json({
+    success: true,
+    message: "Job Created",
+    data: job,
   });
 };
