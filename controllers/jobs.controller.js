7563504ecb0421dd38d75d2fab26dea892c53915
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2023-02-18 22:48:49 -0800

    WIP: Created controller and route to search job withing a user provider zip and distance.

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -1,6 +1,7 @@
 const Job = require("../models/jobs");
-// Get all Jobs => /api/v1/jobs
+const geoCoder = require("../utils/geocoder");
 
+// Get all Jobs => /api/v1/jobs
 exports.getJobs = async (req, res, next) => {
   const jobs = await Job.find();
   res.status(200).json({
@@ -17,5 +18,28 @@ exports.newJob = async (req, res, next) => {
     success: true,
     message: "Job Created",
     data: job,
+  });
+};
+
+// Search jobs with radius => /api/v1/:zipcode/:distance
+exports.getJobsInRadius = async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // gettint latitude and longitude from geocoder with zipcode
+  const loc = await geoCoder.geocode(zipcode);
+  const latitude = loc[0].latitude;
+  const longitude = loc[0].longitude;
+  const radius = distance / 3963;
+
+  const jobs = await Job.find({
+    location: {
+      $geoWithin: { $centerSphere: [[longitude, latitude], radius] },
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    results: jobs.length,
+    data: jobs,
   });
 };
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 // importing Jobs controllers
-const { getJobs, newJob } = require("../controllers/jobs.controller");
+const {
+  getJobs,
+  newJob,
+  getJobsInRadius,
+} = require("../controllers/jobs.controller");
 
 router.route("/jobs").get(getJobs);
+router.route("/jobs/:zipcode/:distance").get(getJobsInRadius);
 router.route("/jobs").post(newJob);
 
 module.exports = router;
