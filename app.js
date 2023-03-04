937620f094ef72c88ec2b6196907ffccc9cac209
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2023-03-03 22:27:20 -0800

    WIP: Setting up unhandled promise and uncaught error handlers.

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const app = express();
+
+// handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down due to uncaught exception`);
+  process.exit(1);
+});
+
 const connectDatabase = require("./config/database");
 const errorMiddleware = require("./middleware/errors");
 
@@ -22,10 +30,19 @@ app.use("/api/v1", jobs);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+// Handling unhandled promise rejection.
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down server due to handled promise rejection.`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
 
 // TODO: Fix issue with scripts commands between *nix OS and Windows OS. check-out cross-env package.
